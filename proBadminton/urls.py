e742bc742a110ac3ab712ff5d49a51bc93eeeb0a
Soumya Ranjan Patra <sonusoum.study@gmail.com>
2023-12-27 11:35:02 +0530

    updated urls.py file

--- proBadminton/urls.py
+++ proBadminton/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
 
     # Orders
     path('orders/', include('orders.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
